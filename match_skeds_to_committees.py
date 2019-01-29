0603736aaee7911ff38ec7b246cd531d72896145
Jacob Fenton <jsfenfen@gmail.com>
2019-01-29 09:59:46 -0800

    wip

--- match_skeds_to_committees.py
+++ match_skeds_to_committees.py
@@ -233,9 +233,9 @@ if __name__ == '__main__':
     committeedict = get_committee_dict()
     candidatedict = get_candidate_dict()
 
-    YEARS = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
+    # YEARS = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
 
-    YEARS = [2007,] # testing
+    YEARS = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
 
     for year in YEARS: 
         process_sked_a(committeedict, candidatedict, year)
