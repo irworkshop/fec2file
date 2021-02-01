d5aa8781e6551fe938dce70ab0ae7c457571d7fb
Jacob Fenton <jsfenfen@gmail.com>
2021-02-01 08:54:25 -0800

    update

--- read_filings_from_amended_headers.py
+++ read_filings_from_amended_headers.py
@@ -40,7 +40,7 @@ legal_skeds = ['A', 'B','F132']
 # To really do sked E we gotta include F57, from the F5's
 
 #ALL_YEARS = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-YEARS = [2019,2020]
+YEARS = [2020, 2021]
 
 
 schedule_writer = {
